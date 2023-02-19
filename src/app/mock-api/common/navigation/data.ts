0b438dc868862d21d11aa053f4f457bf9c0e5a32
Tahemmout Aymane <Tahemmout.Aymane@outlook.com>
2023-02-19 21:25:05 +0100

    Deleted Mock API

--- src/app/mock-api/common/navigation/data.ts
+++ src/app/mock-api/common/navigation/data.ts
@@ -1,39 +1,176 @@
-/* tslint:disable:max-line-length */
 import { FuseNavigationItem } from '@fuse/components/navigation';
 
 export const defaultNavigation: FuseNavigationItem[] = [
     {
-        id   : 'example',
-        title: 'Example',
-        type : 'basic',
-        icon : 'heroicons_outline:chart-pie',
-        link : '/example'
+        id: 'dashboard',
+        title: 'Dashboard',
+        subtitle: 'View your analytics',
+        type: 'basic',
+        icon: 'heroicons_outline:chart-pie',
+        link: '/dashboard'
+    },
+    {
+        id: 'banks',
+        title: 'Banks',
+        subtitle: 'Banks',
+        type: 'basic',
+        icon: 'heroicons_outline:office-building',
+        link: '/banks'
+    },
+    {
+        id: 'users',
+        title: 'Users',
+        subtitle: 'Bank Users',
+        tooltip: 'Bank Users',
+        type: 'basic',
+        icon: 'heroicons_outline:user-group',
+        link: '/users'
+    },
+    {
+        id: 'loaners',
+        title: 'Loaners',
+        subtitle: 'Loaners',
+        type: 'basic',
+        icon: 'heroicons_outline:users',
+        link: '/loaners'
+    },
+    {
+        id: 'loans',
+        title: 'Loans',
+        subtitle: 'Loans',
+        type: 'basic',
+        icon: 'heroicons_outline:currency-dollar',
+        link: '/loans'
     }
 ];
 export const compactNavigation: FuseNavigationItem[] = [
     {
-        id   : 'example',
-        title: 'Example',
-        type : 'basic',
-        icon : 'heroicons_outline:chart-pie',
-        link : '/example'
+        id: 'dashboard',
+        title: 'Dashboard',
+        subtitle: 'View your analytics',
+        type: 'basic',
+        icon: 'heroicons_outline:chart-pie',
+        link: '/dashboard'
+    },
+    {
+        id: 'banks',
+        title: 'Banks',
+        subtitle: 'Banks',
+        type: 'basic',
+        icon: 'heroicons_outline:office-building',
+        link: '/banks'
+    },
+    {
+        id: 'users',
+        title: 'Users',
+        subtitle: 'Bank Users',
+        tooltip: 'Bank Users',
+        type: 'basic',
+        icon: 'heroicons_outline:user-group',
+        link: '/users'
+    },
+    {
+        id: 'loaners',
+        title: 'Loaners',
+        subtitle: 'Loaners',
+        type: 'basic',
+        icon: 'heroicons_outline:users',
+        link: '/loaners'
+    },
+    {
+        id: 'loans',
+        title: 'Loans',
+        subtitle: 'Loans',
+        type: 'basic',
+        icon: 'heroicons_outline:currency-dollar',
+        link: '/loans'
     }
 ];
 export const futuristicNavigation: FuseNavigationItem[] = [
     {
-        id   : 'example',
-        title: 'Example',
-        type : 'basic',
-        icon : 'heroicons_outline:chart-pie',
-        link : '/example'
+        id: 'dashboard',
+        title: 'Dashboard',
+        subtitle: 'View your analytics',
+        tooltip: 'View your analytics',
+        type: 'basic',
+        icon: 'heroicons_outline:chart-pie',
+        link: '/dashboard'
+    },
+    {
+        id: 'banks',
+        title: 'Banks',
+        subtitle: 'Banks',
+        type: 'basic',
+        icon: 'heroicons_outline:office-building',
+        link: '/banks'
+    },
+    {
+        id: 'users',
+        title: 'Users',
+        subtitle: 'Bank Users',
+        tooltip: 'Bank Users',
+        type: 'basic',
+        icon: 'heroicons_outline:user-group',
+        link: '/users'
+    },
+    {
+        id: 'loaners',
+        title: 'Loaners',
+        subtitle: 'Loaners',
+        type: 'basic',
+        icon: 'heroicons_outline:users',
+        link: '/loaners'
+    },
+    {
+        id: 'loans',
+        title: 'Loans',
+        subtitle: 'Loans',
+        type: 'basic',
+        icon: 'heroicons_outline:currency-dollar',
+        link: '/loans'
     }
 ];
 export const horizontalNavigation: FuseNavigationItem[] = [
     {
-        id   : 'example',
-        title: 'Example',
-        type : 'basic',
-        icon : 'heroicons_outline:chart-pie',
-        link : '/example'
+        id: 'dashboard',
+        title: 'Dashboard',
+        subtitle: 'View your analytics',
+        tooltip: 'View your analytics',
+        type: 'basic',
+        icon: 'heroicons_outline:chart-pie',
+        link: '/dashboard'
+    },
+    {
+        id: 'banks',
+        title: 'Banks',
+        subtitle: 'Banks',
+        type: 'basic',
+        icon: 'heroicons_outline:office-building',
+        link: '/banks'
+    },
+    {
+        id: 'users',
+        title: 'Users',
+        subtitle: 'Bank Users',
+        tooltip: 'Bank Users',
+        type: 'basic',
+        icon: 'heroicons_outline:user-group',
+        link: '/users'
+    },
+    {
+        id: 'loaners',
+        title: 'Loaners',
+        subtitle: 'Loaners',
+        type: 'basic',
+        icon: 'heroicons_outline:users',
+        link: '/loaners'
+    },
+    {
+        id: 'loans',
+        title: 'Loans',
+        subtitle: 'Loans',
+        type: 'basic',
+        icon: 'heroicons_outline:currency-dollar',
+        link: '/loans'
     }
 ];
