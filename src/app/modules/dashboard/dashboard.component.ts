bc66ce33ad07bef72e5b00d1c8203e9d8a0061ab
Tahemmout Aymane <Tahemmout.Aymane@outlook.com>
2023-03-05 22:54:17 +0100

    Dashboard Module

---
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -0,0 +1,86 @@
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'app/core/user/user.service';
+import { User } from 'app/core/user/user.types';
+import { ApexOptions } from 'ng-apexcharts';
+import { Subject, takeUntil } from 'rxjs';
+import { DashboardService } from './dashboard.service';
+
+import * as moment from 'moment';
+
+@Component({
+    selector: 'dashboard',
+    templateUrl: './dashboard.component.html'
+})
+export class DashboardComponent implements OnInit, OnDestroy {
+
+    user: User;
+
+    today: number = moment().hour();
+
+    data: any;
+
+    private _unsubscribeAll: Subject<any> = new Subject<any>();
+
+    /**
+     * Constructor
+     */
+    constructor(
+        private _changeDetectorRef: ChangeDetectorRef,
+        private _dashboardService: DashboardService,
+        private _userService: UserService,
+        private _router: Router
+    ) {
+    }
+
+    // -----------------------------------------------------------------------------------------------------
+    // @ Lifecycle hooks
+    // -----------------------------------------------------------------------------------------------------
+
+    /**
+     * On init
+     */
+    ngOnInit(): void {
+        // Subscribe to user changes
+        this._userService.user$
+            .pipe(takeUntil(this._unsubscribeAll))
+            .subscribe((user: User) => {
+                this.user = user;
+
+                // Mark for check
+                this._changeDetectorRef.markForCheck();
+            });
+
+        // Get the data
+        this._dashboardService.data$
+            .pipe(takeUntil(this._unsubscribeAll))
+            .subscribe((data) => {
+
+                // Store the data
+                this.data = data;
+            });
+    }
+
+    /**
+     * On destroy
+     */
+    ngOnDestroy(): void {
+        // Unsubscribe from all subscriptions
+        this._unsubscribeAll.next(null);
+        this._unsubscribeAll.complete();
+    }
+
+    // -----------------------------------------------------------------------------------------------------
+    // @ Public methods
+    // -----------------------------------------------------------------------------------------------------
+
+    /**
+     * Track by function for ngFor loops
+     *
+     * @param index
+     * @param item
+     */
+    trackByFn(index: number, item: any): any {
+        return item.id || index;
+    }
+}
---
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatRippleModule } from '@angular/material/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { NgApexchartsModule } from 'ng-apexcharts';
+import { TranslocoModule } from '@ngneat/transloco';
+import { SharedModule } from 'app/shared/shared.module';
+import { RouterModule } from '@angular/router';
+
+import { DashboardComponent } from './dashboard.component';
+
+import { dashboardRoutes } from './dashboard.routing';
+
+@NgModule({
+    declarations: [
+        DashboardComponent
+    ],
+    imports: [
+        CommonModule,
+        MatButtonModule,
+        MatButtonToggleModule,
+        MatDividerModule,
+        MatIconModule,
+        MatMenuModule,
+        MatProgressBarModule,
+        MatRippleModule,
+        MatSidenavModule,
+        MatSortModule,
+        MatTableModule,
+        MatTabsModule,
+        NgApexchartsModule,
+        TranslocoModule,
+        SharedModule,
+        RouterModule.forChild(dashboardRoutes)
+    ]
+})
+export class DashboardModule { }
